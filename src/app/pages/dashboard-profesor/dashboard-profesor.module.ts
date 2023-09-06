df9c66ecbf65e5b64fa699e5e2fbd0e879d0c2e0
camilaRumino <camila.ruminor@gmail.com>
2023-09-05 23:06:31 -0300

    arreglo 2 de menu

--- src/app/pages/dashboard-profesor/dashboard-profesor.module.ts
+++ src/app/pages/dashboard-profesor/dashboard-profesor.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardProfesorPageRoutingModule } from './dashboard-profesor-routing.module';
 
 import { DashboardProfesorPage } from './dashboard-profesor.page';
+import { Component } from 'ionicons/dist/types/stencil-public-runtime';
 
 @NgModule({
   imports: [
