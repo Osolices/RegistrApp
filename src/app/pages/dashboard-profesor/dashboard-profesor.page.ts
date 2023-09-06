4d8a5b92455072a3cd0f1b330271db5f888fb005
camilaRumino <camila.ruminor@gmail.com>
2023-09-05 23:05:08 -0300

    arreglo de menu

--- src/app/pages/dashboard-profesor/dashboard-profesor.page.ts
+++ src/app/pages/dashboard-profesor/dashboard-profesor.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard-profesor',
@@ -7,29 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardProfesorPage implements OnInit {
 
+  
 
   public clases = [
     {
       id:'1',
+      profesor: 'Víctor Andrade',
+      horario: 'lunes',
       ramo:'Calidad de software'
     },
     {
       id:'2',
+      profesor: 'Andrés Polo',
+      horario: 'martes',
       ramo:'Diseño y prototipos'
     },
     {
       id:'3',
+      profesor: 'Jaqueline Duarte',
+      horario: 'miercoles',
       ramo:'Arquitectura'
     },
     {
       id:'4',
+      profesor: 'Segundo Díaz',
+      horario: 'jueves',
       ramo:'Fé cristiana'
     },
   ];
-  constructor() { }
+
+  
+
+  constructor(private route: Router) { }
+  menuType: string = 'push';
+  
+
+  logOut() {
+    this.route.navigate(['/login']);
+  }
 
   ngOnInit() {
     console.log(this.clases)
+   
   }
+ 
 
 }
+
