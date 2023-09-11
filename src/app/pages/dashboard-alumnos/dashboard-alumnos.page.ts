b33df92f553e4e41c688e6afd7762c823cb3cca1
Osolices <da.santelices@duocuc.cl>
2023-09-11 17:20:17 -0300

    Dash alumnos

--- src/app/pages/dashboard-alumnos/dashboard-alumnos.page.ts
+++ src/app/pages/dashboard-alumnos/dashboard-alumnos.page.ts
@@ -8,6 +8,15 @@ import { Router } from '@angular/router';
 })
 export class DashboardAlumnosPage implements OnInit {
 
+  cards = [
+    {
+      title: 'Card 1',
+      subtitle: 'Subtitle 1',
+      extraInfo: 'Extra info 1',
+      expanded: false
+    },
+    // ... más tarjetas aquí
+  ];
   constructor(private route: Router) { }
 
   menuType: string = 'push';
